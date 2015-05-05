d8b76964550990e049d9bd37261732b81b24092e
jun1007na <junya.n.107+github@gmail.com>
2015-05-05 23:23:00 +0900

    complete character display

--- app/src/main/java/com/example/junya/batterywidget/BatteryWidget.java
+++ app/src/main/java/com/example/junya/batterywidget/BatteryWidget.java
@@ -3,9 +3,13 @@ package com.example.junya.batterywidget;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
+import android.widget.RemoteViews;
 
 /**
  * Created by Junya on 15/05/05.
@@ -20,7 +24,9 @@ public class BatteryWidget extends AppWidgetProvider{
     public static class WidgetService extends Service{
         @Override
         public void onStart(Intent in, int si){
-
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+            registerReceiver(batteryReceiver, filter);
         }
 
         @Override
@@ -28,4 +34,24 @@ public class BatteryWidget extends AppWidgetProvider{
             return null;
         }
     }
+
+    private static BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
+
+        int scale = 100;
+        int level = 0;
+
+        @Override
+        public void onReceive(Context c, Intent in) {
+            String ac = in.getAction();
+            if (ac.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                level = in.getIntExtra("level", 0);
+                scale = in.getIntExtra("scale", 0);
+            }
+            AppWidgetManager awm = AppWidgetManager.getInstance(c);
+            ComponentName cn = new ComponentName(c, BatteryWidget.class);
+            RemoteViews rv = new RemoteViews(c.getPackageName(), R.layout.main);
+            rv.setTextViewText(R.id.textView, "Battery:"+(int)(level*100/scale)+"%");
+            awm.updateAppWidget(cn, rv);
+        }
+    };
 }
